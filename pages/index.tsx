9a55ddd4165638318fe5326d3e40dd720105aba6
acatzk <joshuaimalay@gmail.com>
2021-09-25 22:15:30 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Tiktok - Make Your Day</title>
+      </Head>
+      <h1 className="text-2xl font-semibold text-indigo-800">Hello world</h1>
+    </div>
+  )
+}
+
+export default Index
