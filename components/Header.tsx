2a0c24d5c3aa2363cb25bb42b6733b8a6c2df22b
acatzk <joshuaimalay@gmail.com>
2021-09-26 21:39:59 +0800

    wip: initial main layout (#3)

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,29 +2,33 @@ import React from 'react'
 import Image from 'next/image'
 import { FiSearch } from 'react-icons/fi'
 import { CgMoreVerticalAlt } from 'react-icons/cg'
+import Link from 'next/link'
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 py-1.5 px-3 border-b border-[#D3DEDE] bg-white">
+    <header className="sticky top-0 py-1.5 px-3 border-b border-[#D3DEDE] bg-white z-10">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
         {/* Main Logo */}
-        <div className="relative w-[115px] h-12">
-          <Image
-            src="https://sf16-scmcdn-sg.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/logo-whole-c555aa707602e714ec956ac96e9db366.svg"
-            layout="fill"
-            alt="logo"
-          />
-        </div>
+        <Link href="/">
+          <a className="relative w-[115px] h-12">
+            <Image
+              src="https://sf16-scmcdn-sg.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/logo-whole-c555aa707602e714ec956ac96e9db366.svg"
+              layout="fill"
+              alt="logo"
+            />
+          </a>
+        </Link>
 
         {/* Search */}
         <div
-          className="hidden relative w-1/3 overflow-hidden 
-          md:flex items-center rounded-full bg-[#F1F1F2] border border-transparent group focus-within:border-[#C5C6C9] 
-         transition ease-in-out duration-75">
+          className="hidden relative w-1/3 overflow-hidden md:flex items-center rounded-full 
+                   bg-[#F1F1F2] border border-transparent group focus-within:border-[#C5C6C9] 
+                     transition ease-in-out duration-75">
           <input
             type="text"
             placeholder="Search accounts and videos"
-            className="group text-sm flex-grow py-3 pl-4 mr-11 rounded-full font-normal placeholder-gray-500 text-black bg-[#F1F1F2] focus:outline-none"
+            className="group text-sm flex-grow py-3 pl-4 mr-11 rounded-full font-normal 
+            placeholder-gray-500 text-black bg-[#F1F1F2] focus:outline-none"
           />
           <span className="absolute h-8 w-6 bg-[#dfdfe2] right-[21px]"></span>
           <button className="absolute inset-y-0 right-0 px-3 bg-[#F1F1F2] hover:bg-[#EAEAEB] focus:outline-none">
@@ -37,7 +41,9 @@ const Header: React.FC = () => {
           <a href="#" className="no-underline hover:underline font-semibold text-base">
             Upload
           </a>
-          <button className="px-8 py-2 text-sm font-bold rounded bg-[#fe2c55] hover:bg-[#EF2950] active:bg-[#DF274B] text-white transition ease-in-out duration-150">
+          <button
+            className="px-8 py-2 text-sm font-bold rounded bg-[#fe2c55] hover:bg-[#EF2950] 
+          active:bg-[#DF274B] text-white transition ease-in-out duration-150">
             Log in
           </button>
           <div className="dropdown dropdown-hover dropdown-end">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,70 @@
 import type { NextPage } from 'next'
 import Layout from 'layouts/default'
+import Scrollbar from 'react-smooth-scrollbar'
 
 const Index: NextPage = () => {
   return (
     <Layout>
-      <div className="max-w-5xl mx-auto">
-        <h1 className="text-2xl font-semibold text-indigo-800">Hello world</h1>
+      <div className="relative flex flex-row max-w-5xl mx-auto">
+        <div className="fixed pb-8">
+          <div
+            className="p-4 overflow-y-auto md:h-screen bg-white border-r md:border-transparent shadow md:shadow-none w-[70px] md:w-[350px] 
+          scrollbar-w-2 scrollbar-thumb-rounded-full scrollbar-thumb-gray-300 
+          scrollbar-track-white scrollbar-hidden">
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus,
+              quos veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              orem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus,
+              quos veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
+              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
+            </div>
+          </div>
+        </div>
+        <div className="p-4 md:absolute right-0 w-full md:w-[65%] bg-blue-100 pl-8">
+          orem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
+          veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis mollitia,
+          harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Repudiandae quas, temporibus, quos veritatis aliquid
+          consectetur illo facilis sequi possimus officiis perspiciatis mollitia, harum distinctio
+          repellat aliquam cumque est commodi excepturi.Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Repudiandae quas, temporibus, quos veritatis aliquid consectetur illo
+          facilis sequi possimus officiis perspiciatis mollitia, harum distinctio repellat aliquam
+          cumque est commodi excepturi.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Repudiandae quas, temporibus, quos veritatis aliquid consectetur illo facilis sequi
+          possimus officiis perspiciatis mollitia, harum distinctio repellat aliquam cumque est
+          commodi excepturi.Lorem ipsum orem ipsum dolor sit amet consectetur adipisicing elit.
+          Repudiandae quas, temporibus, quos veritatis aliquid consectetur illo facilis sequi
+          possimus officiis perspiciatis mollitia, harum distinctio repellat aliquam cumque est
+          commodi excepturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          quas, temporibus, quos veritatis aliquid consectetur illo facilis sequi possimus officiis
+          perspiciatis mollitia, harum distinctio repellat aliquam cumque est commodi
+          excepturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas,
+          temporibus, quos veritatis aliquid consectetur illo facilis sequi possimus officiis
+          perspiciatis mollitia, harum distinctio repellat aliquam cumque est commodi
+          excepturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas,
+          temporibus, quos veritatis aliquid consectetur illo facilis sequi possimus officiis
+          perspiciatis mollitia, harum distinctio repellat aliquam cumque est commodi
+          excepturi.Lorem ipsum
+        </div>
       </div>
     </Layout>
   )
