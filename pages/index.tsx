027abbd05583e7b91b5313a442f9fe4559296267
acatzk <joshuaimalay@gmail.com>
2021-09-28 21:50:29 +0800

    wipe: sidebar content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,13 @@
 import type { NextPage } from 'next'
 import Layout from 'layouts/default'
-import Scrollbar from 'react-smooth-scrollbar'
+import Sidebar from 'components/Sidebar'
 
 const Index: NextPage = () => {
   return (
     <Layout>
       <div className="relative flex flex-row max-w-5xl mx-auto">
-        <div className="fixed pb-8">
-          <div
-            className="p-4 overflow-y-auto md:h-screen bg-white border-r md:border-transparent shadow md:shadow-none w-[70px] md:w-[350px] 
-          scrollbar-w-2 scrollbar-thumb-rounded-full scrollbar-thumb-gray-300 
-          scrollbar-track-white scrollbar-hidden">
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus,
-              quos veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              orem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus,
-              quos veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
-              veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis
-              mollitia, harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum
-            </div>
-          </div>
-        </div>
-        <div className="p-4 md:absolute right-0 w-full md:w-[65%] bg-blue-100 pl-8">
+        <Sidebar />
+        <section className="p-4 ml-14 md:absolute right-0 w-full md:w-[65%]">
           orem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas, temporibus, quos
           veritatis aliquid consectetur illo facilis sequi possimus officiis perspiciatis mollitia,
           harum distinctio repellat aliquam cumque est commodi excepturi.Lorem ipsum dolor sit amet
@@ -64,7 +32,7 @@ const Index: NextPage = () => {
           temporibus, quos veritatis aliquid consectetur illo facilis sequi possimus officiis
           perspiciatis mollitia, harum distinctio repellat aliquam cumque est commodi
           excepturi.Lorem ipsum
-        </div>
+        </section>
       </div>
     </Layout>
   )
